0de6b7a18abbaa6ac72ed5bcef80a1a5e31189da
Joao Matos Silva <kappy@acydburne.com.pt>
2014-12-07 16:52:43 +0000

    Added source from tuturial and Autofac integration

---
+++ AuthorizationServer/App_Start/Startup.Autofac.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+using Owin;
+
+namespace AuthorizationServer
+{
+	public partial class Startup
+	{
+	    public void ConfigureAutofac(IAppBuilder app)
+	    {
+	        var builder = new ContainerBuilder();
+
+	        builder.RegisterType<TestService>().AsSelf().InstancePerRequest();
+
+	        var container = builder.Build();
+	        app.UseAutofacMiddleware(container);
+	        app.UseAutofacMvc();
+	        app.UseAutofacWebApi(GlobalConfiguration.Configuration);
+
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+	    }
+	}
+}
